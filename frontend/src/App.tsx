75e87e6c0496410c7772e982bd5eee9d537c692b
IsolatedLun <herojoe210@gmail.com>
2022-02-05 21:24:48 +0200

    Working on navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter as Router } from 'react-router-dom';
+import PrimaryNav from './components/layouts/PrimaryNav';
 
 function App() {
 
   return (
     <Router>
-      
+      <PrimaryNav />
     </Router>
   )
 }
---
+++ frontend/src/components/layouts/PrimaryNav.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PrimaryNav = () => {
+  return(
+      <nav className='primary-nav' role='primary nav'>
+          <h1 className="nav__title">Undef</h1>
+          <div className="nav__user-profile"></div>
+      </nav>
+  )
+};
+
+export default PrimaryNav;
