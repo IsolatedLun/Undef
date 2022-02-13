94eda4e68686ea8a77b47886f77704fff8241cd9
IsolatedLun <herojoe210@gmail.com>
2022-02-13 18:08:52 +0200

    Updated loader

--- frontend/src/components/layouts/Loader.tsx
+++ frontend/src/components/layouts/Loader.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
-
 const Loader = () => {
   return (
     <div className='primary-loader'>
-        <h1>Loading...</h1>
+      <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" 
+        style={{ margin: 'auto', background: 'rgba(0, 0, 0, 0) none repeat scroll 0% 0%', display: 'block', shapeRendering: 'auto'}} width="70px" height="70px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+        <circle cx="50" cy="50" r="41" stroke-width="13" stroke="#ffffff" stroke-dasharray="64.40264939859075 64.40264939859075" fill="none" stroke-linecap="round">
+          <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="0.9174311926605504s" keyTimes="0;1" values="0 50 50;360 50 50"></animateTransform>
+        </circle>
+      </svg>
     </div>
   )
 }
